d1e5b5a5c831a724bb2f4e7e0166823e856ceb44
Elena Petrova <helenofspb@gmail.com>
2023-12-07 17:18:25 +0300

    mfd

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import { legacy_createStore as createStore, combineReducers } from 'redux';
 import { cashReducer } from './cashReducer';
 import { customerReducer } from './customerReducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from '@redux-connected-devtools';
 
 const rootReducer = combineReducers({
     cash: cashReducer,
     customer: customerReducer,
 })
 
-export const store = createStore(rootReducer, composeWithDevTools)
+export const store = createStore(rootReducer, composeWithDevTools())
