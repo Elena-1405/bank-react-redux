c09a3f1a76d8165c8c132515fd81c029568fbe8d
Elena Petrova <helenofspb@gmail.com>
2023-12-07 10:01:04 +0300

    created

---
+++ src/store/customerReducer.js
@@ -0,0 +1,16 @@
+
+
+const defaultState = {
+    customers: [],
+  }
+
+const customerReducer = (state = defaultState, action) => {   //here we define the actions, R is a pure action that takes state and action and returns the new obj of state
+    switch (action.type) {
+      case 'ADD_CUSTOMER':
+        return {...state, cash: state.cash + action.payload}
+      case 'GET_CUSTOMER':
+        return {...state, cash: state.cash - action.payload}
+      default:
+        return state
+    }    
+}
