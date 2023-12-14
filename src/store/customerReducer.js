cef3bc4f21e979b000f906be390dc6f122317fa5
Elena Petrova <helenofspb@gmail.com>
2023-12-14 15:40:09 +0300

    mdf

--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -4,13 +4,19 @@ import React from 'react';
     customers: [],
   }
 
+  const ADD_CUSTOMER = 'ADD_CUSTOMER'
+  const REMOVE_CUSTOMER = 'REMOVE_CUSTOMER'
+
  export const customerReducer = (state = defaultState, action) => {   //here we define the actions, R is a pure action that takes state and action and returns the new obj of state
     switch (action.type) {
-      case 'ADD_CUSTOMER':
+      case ADD_CUSTOMER:
         return {...state, customers: [...state.customers, action.payload]}//возвр новый обж, в кот разворачиваем старое состояние, присваиваем новый массив, в кот разворачиваем уже сущ массив и к нему добавл обж кот прердаем чз action
-      case 'REMOVE_CUSTOMER':
+      case REMOVE_CUSTOMER:
         return {...state, customers: state.customers.filter(customer => customer.id !== action.payload)}
       default:
         return state
     }    
 }
+
+export const addCustomerAction = (payload) => ({type: ADD_CUSTOMER, payload})
+export const removeCustomerAction = (payload) => ({type: REMOVE_CUSTOMER, payload})
